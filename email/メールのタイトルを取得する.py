ee022c0b775cc3321a466581ea3b75d64aaeeb93
Leaner <99289136+learner-peace-fairness-ai@users.noreply.github.com>
2025-03-16 11:50:22 +0900

    Create メールのタイトルを取得する.py

---
+++ email/メールのタイトルを取得する.py
@@ -0,0 +1,18 @@
+import email
+from email.header import decode_header
+from typing import Union
+
+
+def get_mail_subject(mail: Union[email.message.EmailMessage, email.message.Message]):
+    """メールのタイトルを取得する
+
+    Parameters:
+        mail (email.message.EmailMessage | email.message.Message)
+
+    Returns:
+        str: タイトル."""
+    
+    subject, encoding = decode_header(mail["Subject"])[0]
+    if isinstance(subject, bytes):
+        subject = subject.decode(encoding if encoding else "utf-8")
+    return subject
