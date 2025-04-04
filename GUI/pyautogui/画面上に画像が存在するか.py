45c1f643bfa8804b222c50440bac816863f713aa
Leaner <99289136+learner-peace-fairness-ai@users.noreply.github.com>
2025-04-04 09:25:33 +0900

    Update 画面上に画像が存在するか.py

--- GUI/pyautogui/画面上に画像が存在するか.py
+++ GUI/pyautogui/画面上に画像が存在するか.py
@@ -1,7 +1,7 @@
 # ※ OpenCVをインストールしておく必要がある
 def exists_image_on_screen(image_path, confidence: float = 0.9):
-    locations = list(pyautogui.locateAllOnScreen(image_path, confidence=confidence))
-    if locations:
+    try:
+        pyautogui.locateOnScreen(image_path, confidence=confidence)
         return True
-    else:
+    except:
         return False
